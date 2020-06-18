ac4d1a7d0cf4c0bac53bd20720b3eb82280a565a
Landry Argabright <landryargabright1@gmail.com>
2020-06-18 10:51:06 -0500

    UI Complete

---
+++ LoginBuild/ViewControllers/LoginViewController.swift
@@ -0,0 +1,66 @@
+//
+//  LoginViewController.swift
+//  LoginBuild
+//
+//  Created by Ray Argabright on 6/18/20.
+//  Copyright © 2020 Landry Argabright. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: ViewController {
+
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var goButton: UIButton!
+    
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        setUpElements()
+    }
+    
+
+    override func setUpElements() {
+        
+        // Hide error label
+        errorLabel.alpha = 0
+        
+        // Style text field items
+        for tf in [emailTextField, passwordTextField] {
+            
+            Utilities.styleTextField(tf!)
+        }
+        
+        // Style filled button
+        Utilities.styleFilledButton(goButton)
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    
+    @IBAction func goTapped(_ sender: Any) {
+        
+        
+    }
+    
+    
+    
+}
--- LoginBuild/ViewControllers/ViewController.swift
+++ LoginBuild/ViewControllers/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var signupButton: UIButton!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        setUpElements()
     }
 
+    
+    func setUpElements() {
+        
+        // Set button styles
+        Utilities.styleFilledButton(signupButton)
+        Utilities.styleHollowButton(loginButton)
+        
+    }
 
 }
 
